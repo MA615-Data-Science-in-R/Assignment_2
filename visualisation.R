7f9b2ef63f0fd326e7acc78f46348e1f1bf5d6d9
guangze-yu <gzyu@ucdavis.edu>
2021-10-28 11:05:07 -0400

    .

--- visualisation.R
+++ visualisation.R
@@ -1,7 +1,5 @@
 library(ggplot2)
 
-source(file = "/Users/zarawaheed/Documents/BostonUniversity/MA615/Homework/Assignment_2/wrangling_code.R", echo = TRUE)
-
 # Use ggplot to display the results
 # geom_point assigns each country as points and within it we assign the size of each point
 # labs is used to name the x and y axis as well as a main heading
@@ -9,15 +7,31 @@ source(file = "/Users/zarawaheed/Documents/BostonUniversity/MA615/Homework/Assig
 
 # Plot the results for 2020
 
-fig_2020 <- ggplot(data_2020, aes(births, mort_rt)) + 
-  geom_point(size = 2) +
-  labs(x = "Babies per Woman", y = "Mortality Rates", main = "Effect of Babies per Woman on Mortality rates (2020)") +
-  geom_smooth(method = lm, colour = "red")
+vis_fun_2020 <- function(df){
+  p2020 <- ggplot(df,aes(births, mort_rt)) + 
+    geom_point(size = 2) +
+    labs(x = "Babies per Woman", y = "Mortality Rates", main = "Effect of Babies per Woman on Mortality rates (2020)") +
+    geom_smooth(method = lm, colour = "red")
+  return(p2020)
+}
+
+### fig_2020 <- ggplot(data_2020, aes(births, mort_rt)) + 
+###  geom_point(size = 2) +
+###  labs(x = "Babies per Woman", y = "Mortality Rates", main = "Effect of Babies per Woman on Mortality rates (2020)") +
+###  geom_smooth(method = lm, colour = "red")
 
 # Plot teh results for 2021
 
-fig_2021 <- ggplot(data_2021, aes(births, mort_rt)) + 
-  geom_point(size = 2) +
-  labs(x = "Babies per Woman", y = "Mortality Rates", main = "Effect of Babies per Woman on Mortality rates (2021)") +
-  geom_smooth(method = lm, colour = "pink")
+vis_fun_2021 <- function(df){
+  p2021 <- ggplot(df,aes(births, mort_rt)) + 
+    geom_point(size = 2) +
+    labs(x = "Babies per Woman", y = "Mortality Rates", main = "Effect of Babies per Woman on Mortality rates (2021)") +
+    geom_smooth(method = lm, colour = "pink")
+  return(p2021)
+}
+
+### fig_2021 <- ggplot(data_2021, aes(births, mort_rt)) + 
+###  geom_point(size = 2) +
+###  labs(x = "Babies per Woman", y = "Mortality Rates", main = "Effect of Babies per Woman on Mortality rates (2021)") +
+###  geom_smooth(method = lm, colour = "pink")
   
